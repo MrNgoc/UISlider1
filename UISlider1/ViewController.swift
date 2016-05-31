38da5bbb837f0b483d9adb97c3c39f8bedcf8f54
MrNgoc <admin@Admins-MacBook-Pro.local>
2016-05-31 12:18:07 +0700

    Uislider

--- UISlider1/ViewController.swift
+++ UISlider1/ViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bt_Play: UIButton!
+    var audio = AVAudioPlayer()
+    var check: Int!
+    
+    @IBOutlet weak var slider_Volume: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        check = 0
+        audio = try! AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath :NSBundle.mainBundle().pathForResource("music", ofType: ".mp3")!))
+        audio.prepareToPlay()
+        addThumImgForSlider()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func addThumImgForSlider() {
+        slider_Volume.setThumbImage(UIImage(named:"thumb.png"), forState: .Normal)
+        slider_Volume.setThumbImage(UIImage(named:"thumbHightLight.png"), forState: .Highlighted)
     }
+    @IBAction func action_Play(sender: AnyObject) {
+        
+        if check == 0 {
+            audio.play()
+            bt_Play.setImage(UIImage(named:"pause.png"), forState: .Normal)
+            check = 1
+            
+        } else {
+        audio.pause()
+            bt_Play.setImage(UIImage(named:"play.png"), forState: .Normal)
+            
+        check = 0
+    }
+    }
+    @IBAction func Slider(sender: UISlider) {
+        audio.volume = sender.value
+    }
+ 
 
 
 }
-
